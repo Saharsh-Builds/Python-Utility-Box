647c94750eec453044b1592b9ab06d502edcf2b6
Saharsh <saharsh@Saharshs-Mac-mini.local>
2026-06-19 22:14:37 +0530

    Added 1st part of interactive menu in main.py

--- main.py
+++ main.py
@@ -1,1 +1,61 @@
-print("Hey this is a test file!!")
+import questionary
+import sys
+
+def main_menu():
+    while True:
+        
+        custom_style = questionary.Style([
+        ('highlighted', 'bold'),
+        ])
+
+        Choice = questionary.select(
+            "Please select an Utility tool and press Enter to continue: ",
+            choices=[
+                "1. Calculator",
+                "2. Dice Roller",
+                "3. Password Generator",
+                "4. Exit"
+            ],
+            pointer= ">",
+            style=custom_style
+        ).ask()
+
+        return Choice
+
+        
+def main():
+    while True:
+        
+        
+        UserChoice = main_menu()
+
+
+        if UserChoice == "1. Calculator":
+            from calculator import calculator
+            calculator()
+
+        elif UserChoice == "2. Dice Roller":
+            from diceroller import dice_roller
+            dice_roller()
+        
+        elif UserChoice == "3. Password Generator":
+            from pw_gen import password_generator
+            password_generator()
+
+        elif UserChoice == "4. Exit":
+            print("Exiting the program. Goodbye!")
+            sys.exit()
+
+        else:
+            print("How did you get here? Please select a valid option from the menu.")
+            main_menu()
+
+
+
+
+
+
+
+
+
+main()
